824843947fc1e535034755a39f8941e0cba52f83
mrclouddev <166690757+mrcloudddev@users.noreply.github.com>
2026-06-04 17:24:39 +0700

    Update script.js

--- siswa/script.js
+++ siswa/script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbxRRbJTnNlGX9aJJVJFwoxsOKoHa9GVQ7CUJb4Jk_YsXeanI1P08bZ0eelbYjjxoJ22XA/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwBiHVR5WZEgcgHY-a7XqPYHMedZOhTB8ZpzTZQ_P77-8FtJbap92w4vuFrN-8wJTouvg/exec";
 
 let sessionToken = "";
 let examQuestions = [];
